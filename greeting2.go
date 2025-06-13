0aaef347592169c953f3dba37688828f18d01fcd
ParaneoSun <sparaneo@gmail.com>
2025-06-13 16:41:33 +0900

    Create greeting2.go

---
+++ greeting2.go
@@ -0,0 +1,19 @@
+package greeting2
+
+import "time"
+
+func Do() string {
+	//現在時刻を取得
+	current_time := time.Now()
+	var greet_str string
+
+	switch {
+	case current_time.Hour() >= 4 && current_time.Hour() < 10:
+		greet_str = "おはよう" + current_time.String()
+	case current_time.Hour() >= 10 && current_time.Hour() < 17:
+		greet_str = "こんにちは" + current_time.String()
+	case current_time.Hour() >= 17:
+		greet_str = "こんばんは" + current_time.String()
+	}
+	return greet_str
+}
