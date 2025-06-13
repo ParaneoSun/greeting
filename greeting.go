89be759c0401e1602c2c6912e2a82d42736e2d2a
ParaneoSun <sparaneo@gmail.com>
2025-06-13 17:19:43 +0900

    Update greeting.go

--- greeting.go
+++ greeting.go
@@ -9,11 +9,11 @@ func Do() string {
 
 	switch {
 	case current_time.Hour() >= 4 && current_time.Hour() < 10:
-		greet_str = "おはよう" + current_time.String()
+		greet_str = "おはよう" + string(current_time.Hour())
 	case current_time.Hour() >= 10 && current_time.Hour() < 17:
-		greet_str = "こんにちは" + current_time.String()
+		greet_str = "こんにちは" + string(current_time.Hour())
 	case current_time.Hour() >= 17:
-		greet_str = "こんばんは" + current_time.String()
+		greet_str = "こんばんは" + string(current_time.Hour())
 	}
 	return greet_str
 }
